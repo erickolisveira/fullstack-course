6e65eb3e7622e97b6af2ed65d504e70fe5bdcabc
Elderick <erickoliveiraah@gmail.com>
2019-10-14 19:54:37 -0300

    Add update method to axios module

--- part2/phonebook/src/services/phones.js
+++ part2/phonebook/src/services/phones.js
@@ -6,14 +6,18 @@ const getAll = () => {
    .then(response => response.data);
 }
 
-const create = newPhone => {
-   const request = axios.post(baseUrl, newPhone);
+const create = newPersons => {
+   const request = axios.post(baseUrl, newPersons);
    return request.then(response => response.data);
 }
 
 const remove = id => {
-   return axios.delete(`${baseUlr}/${id}`)
-   .then(res => console.log(res));
+   return axios.delete(`${baseUrl}/${id}`)
 }
 
-export default { getAll, create, remove }
+const update = (id, changedPerson) => {
+   const request = axios.put(`${baseUrl}/${id}`, changedPerson);
+   return request.then(response => response.data);
+}
+
+export default { getAll, create, remove, update }
